ed26b109a62b994705cd71a857bd206841982fe2
fpetrakov <jakephin2015@gmail.com>
2023-08-08 20:08:11 +0300

    prediction based on prediction

--- chapter_3_pred_from_pred.py
+++ chapter_3_pred_from_pred.py
@@ -13,7 +13,8 @@ def vect_mat_mul(vect, matrix):
 
 
 def neural_network(input, weights):
-    pred = vect_mat_mul(input, weights)
+    hid = vect_mat_mul(input, weights[0])
+    pred = vect_mat_mul(hid, weights[1])
     return pred
 
 
@@ -22,9 +23,15 @@ wlrec = np.array([0.65, 0.8, 0.8, 0.9])
 nfans = np.array([1.2, 1.3, 0.5, 1.0])
 
 input = np.array([toes[0], wlrec[0], nfans[0]])
-weights = np.array(
+
+ih_weights = np.array(
     [np.array([0.1, 0.2, 0]), np.array([0.1, 0.1, -0.3]), np.array([0.0, 1.3, 0.1])]
 )
+hp_weights = np.array(
+    [np.array([0.1, 0.2, 0]), np.array([0.1, 0.1, -0.3]), np.array([0.0, 1.3, 0.1])]
+)
+
+weights = [ih_weights, hp_weights]
 
 pred = neural_network(input, weights)
 print(pred)
