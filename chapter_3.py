37df93747ae2f91ef3a4f127ba14f061f5cf8348
fpetrakov <jakephin2015@gmail.com>
2023-08-08 19:46:28 +0300

    chapt 3 numpy version

--- chapter_3.py
+++ chapter_3.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def w_sum(a, b):
 	assert(len(a) == len(b))
 
@@ -10,15 +12,16 @@ def w_sum(a, b):
 
 
 def neural_network(input, weights):
-	pred = w_sum(input, weights)
+	# dot - dot product - скалярное произведение
+	pred = input.dot(weights)
 	return pred
 
-toes = [8.5, 9.5, 9.9, 9.0]
-wlrec = [0.65, 0.8, 0.8, 0.9]
-nfans = [1.2, 1.3, 0.5, 1.0]
+toes = np.array([8.5, 9.5, 9.9, 9.0])
+wlrec = np.array([0.65, 0.8, 0.8, 0.9])
+nfans = np.array([1.2, 1.3, 0.5, 1.0])
 
-input = [toes[0], wlrec[0], nfans[0]]
-weights = [0.1, 0.2, 0]
+input = np.array([toes[0], wlrec[0], nfans[0]])
+weights = np.array([0.1, 0.2, 0])
 
 pred = neural_network(input, weights)
 print(pred)
